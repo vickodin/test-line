548c6bec57e09d7d1b64dd9b7740c1e2aeb84490
vickodin <vick.orel@gmail.com>
2013-06-28 14:17:59 +0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestLine::Application.config.secret_token = '410710912dc6fc492587140f88cda9090d3ec8e6fbc953eaa3097fb066a866572036dc76e119201cb6ea4f629121c76641e99986c9a2ab332f8c93a6bf973c6d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+TestLine::Application.config.session_store :cookie_store, key: '_test-line_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# TestLine::Application.config.session_store :active_record_store
