f1025674c62f1a0603d28942209216ea574daac2
vickodin <vick.orel@gmail.com>
2013-06-28 14:25:54 +0400

    Added Line model + home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 TestLine::Application.routes.draw do
+  post "/home/create", :as => :new_line
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +49,7 @@ TestLine::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
